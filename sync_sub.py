716308efcbcf561645753048e33726095a1886c7
rudenoise <joel@hughesindustries.uk>
2023-06-13 19:47:35 +0100

    start building up sync

--- sync_sub.py
+++ sync_sub.py
@@ -1,2 +1,16 @@
 from wave_slice.io import fresh_setup, read_to_rosa, write_file
 from wave_slice.proc import norm, regularise, separate_at_zero_crossings
+
+sample_rate = 48000
+
+# name the files
+bass_file = "2020-06-12-BG"
+sub_file = "2020-06-12-SerumSUB.wav"
+
+# load them using librosa
+bass_file = read_to_rosa(bass_file, sample_rate)
+sub_file = read_to_rosa(sub_file, sample_rate)
+
+# parcel up into sections, split on zero crossings
+bass_sections = separate_at_zero_crossings(bass_file)
+sub_sections = separate_at_zero_crossings(sub_file)
